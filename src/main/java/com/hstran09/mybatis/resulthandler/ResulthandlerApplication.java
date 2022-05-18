2186f4e21ca1c5826f4c1bbe0d39d9c4c6565dad
tran-h1 <h-tran@usen-next.jp>
2022-05-18 18:39:16 +0700

    init source

---
+++ src/main/java/com/hstran09/mybatis/resulthandler/ResulthandlerApplication.java
@@ -0,0 +1,13 @@
+package com.hstran09.mybatis.resulthandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResulthandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResulthandlerApplication.class, args);
+	}
+
+}
